0b25110374b93d972bf64201ada9fae20b964f02
Eliseo <eliseo@Nidias-MacBook-Air.local>
2022-10-24 21:01:40 -0500

    added Js search bar

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var searchButton = document.getElementById('searchButton')
+var searchInputn = document.getElementById('searchInput')
+
+
+
+
+searchButton.addEventListener('click', function(e){
+    e.preventDefault();
+    const  userSearchInput = searchInput.value
+    getMoVie( ,userSearchInput)
+});
